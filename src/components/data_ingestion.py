dbbf2df2c41e8bb5ea7df64cde3341b5eccd9f78
Nadagani Amulya Prasanth <75437956+amulyaprasanth@users.noreply.github.com>
2023-08-15 00:30:14 +0530

    model trainer and hyperparameter tuning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,15 +5,18 @@ import pandas as pd
 from src.logger import logging
 from src.exception import CustomException
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
 
 @dataclass
 class DataIngestionConfig:
     raw_data_path: str = os.path.join('artifacts', 'data.csv')
     train_data_path: str = os.path.join('artifacts', 'train.csv')
     test_data_path: str = os.path.join('artifacts', 'test.csv')
+
 
 class DataIngestion:
     def __init__(self):
@@ -26,7 +29,8 @@ class DataIngestion:
             data = pd.read_csv('data\data_cleaned.csv')
             logging.info("Read the dataset as a dataframe")
 
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+            os.makedirs(os.path.dirname(
+                self.ingestion_config.train_data_path), exist_ok=True)
             data.to_csv(self.ingestion_config.raw_data_path,
                         index=False,
                         header=True)
@@ -52,9 +56,14 @@ class DataIngestion:
         except Exception as e:
             raise CustomException(e, sys)
 
+
 if __name__ == "__main__":
     obj = DataIngestion()
     train_set, test_set = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_set, test_set)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(
+        train_set, test_set)
+
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
--- src/utils.py
+++ src/utils.py
@@ -1,25 +1,71 @@
-import os 
+import os
 import sys
-import dill
 import pickle
+import numpy as np
 
-
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
+from src.logger import logging
+
 
 def save_object(file_path, obj):
-    try: 
+    try:
         dir_path = os.path.dirname(file_path)
         os.makedirs(dir_path, exist_ok=True)
-        
+
         with open(file_path, "wb") as f:
             pickle.dump(obj, f)
     except Exception as e:
         raise CustomException(e, sys)
-    
+
+
 def load_object(file_path):
     try:
         with open(file_path, "rb") as f:
             return pickle.load(file_path)
-        
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
+    try:
+        report = {}
+
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            para = params[list(params.keys())[i]]
+
+            gs = GridSearchCV(model, param_grid=para, cv=3, n_jobs=-1)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+            train_preds = model.predict(X_train)
+            test_preds = model.predict(X_test)
+
+            train_rmse = np.sqrt(mean_squared_error(y_train, train_preds))
+            train_mae = mean_absolute_error(y_train, train_preds)
+            train_r2 = r2_score(y_train, train_preds)
+
+            test_rmse = np.sqrt(mean_squared_error(y_test, test_preds))
+            test_mae = mean_absolute_error(y_test, test_preds)
+            test_r2 = r2_score(y_test, test_preds)
+
+            logging.info(f"""   Model: {list(models.keys())[i]}
+                                Metrics (train_score, test_score)
+                                rmse: {train_rmse, test_rmse}
+                                mae : {train_mae, test_mae}
+                                r2  : {train_r2, test_r2} """)
+
+            report[list(models.keys())[i]] = test_rmse
+
+        return (
+            model,
+            report
+        )
+
     except Exception as e:
         raise CustomException(e, sys)
